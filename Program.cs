4315ba8300524c2e69eaae91137118fc8ec47748
Able <134662899+Ablebangbang@users.noreply.github.com>
2023-06-02 02:30:21 +0800

    百元買百雞

--- Program.cs
+++ Program.cs
@@ -1,87 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine(DrawTriangle(5));
-            // Console.WriteLine(DrawTriangleB(5));
-            // Console.WriteLine(DrawTriangleC(5));
-
-            Func<int, int, string> funcLeft = (rows, currentRow) => new string('*', currentRow);
-            Func<int, int, string> funcRight = (rows, currentRow) => new string('*', currentRow).PadLeft(rows);
-            Func<int, int, string> funcCenter = (rows, currentRow) 
-            => new string(' ', rows - currentRow) + new string('*', 2 * currentRow - 1);
-
-            //倒正triangle
-            Func<int, int, string> funcInvertedCenter = (rows, currentRow) =>  new string(' ', currentRow - 1) + new string('*', 2 * (rows - currentRow) + 1);
-
-            //倒左triangle
-            Func<int, int, string> funcInvertedLeft = (rows, currentRow) =>  new string('*', rows - currentRow + 1);
-
-            //倒右triangle
-            Func<int, int, string> funcInvertedRight = (rows, currentRow) => new string('*', rows - currentRow + 1).PadLeft(rows);
-
-            Console.WriteLine(DrawTriangleZ(5, funcLeft));
-            Console.WriteLine(DrawTriangleZ(5, funcRight));
-            Console.WriteLine(DrawTriangleZ(5, funcCenter));
-            Console.WriteLine(DrawTriangleZ(5, funcInvertedLeft));
-            Console.WriteLine(DrawTriangleZ(5, funcInvertedRight));
-            Console.WriteLine(DrawTriangleZ(5, funcInvertedCenter));
+            Func<int, int, int, int, int, string> ChickenNum = (cockP, henP, chickenP, totalp, totalnum) => "";
+            Console.WriteLine(BuyChicken(5, 3, 1, 100, 100, ChickenNum));
         }
 
-        static string DrawTriangleZ(int rows, Func<int, int, string> rowFunc)
+
+        public static string BuyChicken(int cockP, int henP, int chickenP, int totalp, int totalnum, Func<int, int, int, int, int, string> Handler)
         {
             string result = string.Empty;
-
-            for (int i = 1; i <= rows; i++)
-            {
-                result += rowFunc(rows, i) + "\n";
-            }
-
+            Enumerable.Range(1, totalp / cockP).ToList().ForEach(cock =>
+                   Enumerable.Range(1, totalp / henP).ToList().ForEach(hen =>
+                   {
+                       int chicken = totalnum - cock - hen;
+                       if (cock * cockP + hen * henP + (chicken * chickenP) / 3 == totalp && chicken % 3 == 0)
+                       {
+                           result += $"公雞：{cock} 隻，母雞：{hen} 隻，小雞：{chicken} 隻\n";
+                           //result += Handler(cockP, henP, chickenP) ;
+                       }
+                   })
+               );
             return result;
         }
-        /*static string DrawTriangle(int rows) // , Func<int, int, string> rowFunc)
-        {
-            string result = string.Empty;
-
-            for(int i=1; i<= rows; i++)
-            {
-                result += new string('*', i) + "\n";
-            }
-
-            return result;
-        }
-
-        static string DrawTriangleB(int rows)
-        {
-            string result = string.Empty;
-
-            for (int i = 1; i <= rows; i++)
-            {
-                result += new string('*', i).PadLeft(rows) + "\n";
-            }
-
-            return result;
-        }
-
-        static string DrawTriangleC(int rows)
-        {
-            string result = string.Empty;
-
-            for (int i = 1; i <= rows; i++)
-            {
-                result += new string(' ', rows-i) + new string('*', 2*i-1) + "\n";
-            }
-
-            return result;
-        }*/
     }
 }
+
+
