d3f5b6046d0790db2634eb93758c402a86c39d68
Able <134662899+Ablebangbang@users.noreply.github.com>
2023-06-02 09:28:03 +0800

    chicken.cs

--- chicken.cs
+++ chicken.cs
@@ -8,14 +8,14 @@ namespace ConsoleApp3
 {
     internal class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Func<int, int, int, int, int, string> ChickenNum = (cockP, henP, chickenP, totalp, totalnum) => "";
+            Func<int, int, int, int, int, string> ChickenNum = (cock, hen, chicken, totalp, totalnum) => $"Cock:{cock}隻,Hen:{hen}隻,Chicken{chicken}隻\n";
             Console.WriteLine(BuyChicken(5, 3, 1, 100, 100, ChickenNum));
         }
 
 
-        public static string BuyChicken(int cockP, int henP, int chickenP, int totalp, int totalnum, Func<int, int, int, int, int, string> Handler)
+        public static string BuyChicken(int cockP, int henP, int chickenP, int totalp, int totalnum, Func<int, int, int, int, int, string>Handler)
         {
             string result = string.Empty;
             Enumerable.Range(1, totalp / cockP).ToList().ForEach(cock =>
@@ -24,14 +24,12 @@ namespace ConsoleApp3
                        int chicken = totalnum - cock - hen;
                        if (cock * cockP + hen * henP + (chicken * chickenP) / 3 == totalp && chicken % 3 == 0)
                        {
-                           result += $"公雞：{cock} 隻，母雞：{hen} 隻，小雞：{chicken} 隻\n";
-                           //result += Handler(cockP, henP, chickenP) ;
+                          // result += $"公雞：{cock} 隻，母雞：{hen} 隻，小雞：{chicken} 隻\n";
+                           result += Handler(cock, hen, chicken, totalp, totalnum) ;
                        }
                    })
                );
             return result;
         }
-    }
-}
 
 
